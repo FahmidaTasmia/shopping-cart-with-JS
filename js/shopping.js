0d63ee9fa83970c4eedc19bc75961e190e83a9af
FahmidaTasmia <tasmia1070@gmail.com>
2022-08-20 13:49:25 +0600

    product cart calculation added

--- js/shopping.js
+++ js/shopping.js
@@ -1,7 +1,47 @@
-const cartArray=[];
+const cart = [];
+// products-cart
 
-function addToCart(element){
-    const productName = element.parentNode.parentNode.children[0].innerText;
-    const productPrice = element.parentNode.parentNode.children[1].innerText;
+function displayProduct()
+{
+    let totalPrice = 0;
+    const totalProducts = document.getElementById("total-products");
+    totalProducts.innerText = cart.length;
 
+    const cartContainer = document.getElementById("products-cart");
+    cartContainer.textContent = '';
+
+    for(let i = 0; i < cart.length; i++)
+    {
+        totalPrice += cart[i].pdPrice;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${cart[i].pdName}</td>
+        <td>${cart[i].pdPrice}</td>
+        `;
+        cartContainer.appendChild(tr);
+    }
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th></th>
+    <td>Total Price</td>
+    <td>${totalPrice}</td>
+    `;
+
+    cartContainer.appendChild(tr);
+}
+
+function addToCart(element) {
+    const pdName = element.parentNode.parentNode.children[0].innerText;
+    const pdPrice = element.parentNode.parentNode.children[1].children[0].innerText;
+
+    const pd = {
+        pdName: pdName,
+        pdPrice: parseInt(pdPrice)
+    }
+
+    cart.push(pd);
+    
+    displayProduct();
 }
