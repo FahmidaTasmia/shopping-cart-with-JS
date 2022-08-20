170ebc4da926c55f447bb65a3290f5b7a7fa3c7c
FahmidaTasmia <tasmia1070@gmail.com>
2022-08-20 12:42:47 +0600

    js file added

---
+++ js/shopping.js
@@ -0,0 +1,7 @@
+const cartArray=[];
+
+function addToCart(element){
+    const productName = element.parentNode.parentNode.children[0].innerText;
+    const productPrice = element.parentNode.parentNode.children[1].innerText;
+
+}
